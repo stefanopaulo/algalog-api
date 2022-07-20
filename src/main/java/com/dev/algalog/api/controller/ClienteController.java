3f2269ccab53e4d791b949239131f2746a63ef28
Stefano Paulo <stefanopaulo47@gmail.com>
2022-07-20 16:59:38 -0300

    Implementando Collection Resource

--- src/main/java/com/dev/algalog/api/controller/ClienteController.java
+++ src/main/java/com/dev/algalog/api/controller/ClienteController.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.algalog.domain.model.Cliente;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		Cliente cliente1 = new Cliente(1L, "João", "joaodascouves@gmail.com", "34 99999-1111");
 		Cliente cliente2 = new Cliente(2L, "Maria", "mariadasneves@gmail.com", "11 97777-4343");
